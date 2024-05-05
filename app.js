9f15d66fe44e3dea0f12de37954ce8a297232d7d
ClavijoDiego <diego.clavijo@outlook.es>
2024-05-05 14:33:35 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a different kind of hello'));
+app.get('/', (req, res) => res.send('I have completely changed this again'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
